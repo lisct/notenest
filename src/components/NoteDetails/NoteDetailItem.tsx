c5784d7236367edbbc7ce59db8d1995a61e07f19
Lisbel Cruz <Lcruztaveras@gmail.com>
2025-03-18 16:14:21 -0500

    Add NoteDetailItem component

---
+++ src/components/NoteDetails/NoteDetailItem.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import { Accordion, Text, Heading, Box } from "@chakra-ui/react";
+import { formattedDate } from "../../utils/date";
+
+type Props = {
+  title: string;
+  content: string;
+  date: string;
+};
+
+const NoteDetailItem: FC<Props> = ({ title, content, date }) => {
+  return (
+    <Accordion.Item value={title} padding="md" boxShadow="lg" borderRadius="lg" marginBottom="md">
+      <Accordion.ItemTrigger>
+        <Box width="100%" display="flex" gap="2xs" flexDirection={{ base: "column", sm: "row" }}>
+          <Heading fontWeight="400" size="lg" flex="1">
+            {title}
+          </Heading>
+          <Text color="gray.100">{formattedDate(date)}</Text>
+        </Box>
+        <Accordion.ItemIndicator />
+      </Accordion.ItemTrigger>
+      <Accordion.ItemContent>
+        <Accordion.ItemBody>
+          <Text color="#637381" fontSize="14px">
+            {content}
+          </Text>
+        </Accordion.ItemBody>
+      </Accordion.ItemContent>
+    </Accordion.Item>
+  );
+};
+
+export default NoteDetailItem;
