533b7f1100953177904ab5d9713e1a00aa5bfdd5
Lisbel Cruz <Lcruztaveras@gmail.com>
2025-03-18 14:36:32 -0500

    Update Routes with detailPage component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import DetailPage from "./pages/DetailPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const AppRoutes = () => {
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/details" element={<DetailPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
