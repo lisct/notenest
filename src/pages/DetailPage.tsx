9c025816dbd06bce33e3cfd3e535bb66b62db10a
Lisbel Cruz <Lcruztaveras@gmail.com>
2025-03-18 16:15:01 -0500

    Add created notes from localStorage in DetailPage

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,8 +1,19 @@
+import { useState, useEffect } from "react";
+import compact from "lodash/compact";
 import { Box, Heading, Separator } from "@chakra-ui/react";
-import PageContainer from "../components/PageContainer/PageContainer";
+import { getLocalStorageItem, Note as NoteType } from "../components/lib/localStorage";
 import CreateNoteModal from "../components/CreateNoteModal/CreateNoteModal";
+import PageContainer from "../components/PageContainer/PageContainer";
+import NoResult from "../components/NoResult/NoResult";
+import NoteDetails from "../components/NoteDetails/NoteDetails";
 
 const DetailPage = () => {
+  const [entries, setEntriesState] = useState<NoteType[]>();
+
+  useEffect(() => {
+    setEntriesState(getLocalStorageItem());
+  }, []);
+
   return (
     <PageContainer>
       <Box display="flex" flexDirection="column" gap="3xl">
@@ -13,6 +24,14 @@ const DetailPage = () => {
           </Box>
           <Separator />
         </Box>
+
+        {compact(entries).length > 0 ? (
+          <Box display="flex" flexDirection="column" gap="sm">
+            {entries && <NoteDetails notes={entries} />}
+          </Box>
+        ) : (
+          <NoResult />
+        )}
       </Box>
     </PageContainer>
   );
