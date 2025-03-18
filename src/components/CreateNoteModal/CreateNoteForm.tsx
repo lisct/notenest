2648b849229bcd570a2a0114cfb8d7b5278696d9
Lisbel Cruz <Lcruztaveras@gmail.com>
2025-03-18 15:10:13 -0500

    Add CreateNoteForm component + validations

---
+++ src/components/CreateNoteModal/CreateNoteForm.tsx
@@ -0,0 +1,74 @@
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { Text, Field, Input, Stack, Button, Flex } from "@chakra-ui/react";
+
+type FormValues = {
+  title: string;
+  content: string;
+};
+
+type Props = {
+  onClose: () => void;
+};
+
+const NoteForm: FC<Props> = ({ onClose }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormValues>({
+    mode: "onChange",
+  });
+
+  const handleCreateNote = (data: FormValues) => {
+    console.log(data);
+    onClose();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleCreateNote)}>
+      <Stack gap="md">
+        <Field.Root>
+          <Field.Label>Title</Field.Label>
+          <Input
+            {...register("title", {
+              required: "Title is required",
+              maxLength: {
+                value: 50,
+                message: "Title cannot be longer than 50 characters",
+              },
+            })}
+            placeholder="Title"
+          />
+          {errors.title && <Text color="red.100">{errors.title.message}</Text>}
+        </Field.Root>
+        <Field.Root>
+          <Field.Label>Content</Field.Label>
+          <Input
+            {...register("content", {
+              required: "Content is required",
+              maxLength: {
+                value: 200,
+                message: "Content cannot be longer than 200 characters",
+              },
+            })}
+            placeholder="Content"
+          />
+          {errors.content && <Text color="red.100">{errors.content.message}</Text>}
+        </Field.Root>
+
+        <Flex justifyContent="flex-end" gap="xs">
+          <Button variant="outline" onClick={onClose}>
+            Cancel
+          </Button>
+
+          <Button background="red.100" type="submit" disabled={!isValid}>
+            Create
+          </Button>
+        </Flex>
+      </Stack>
+    </form>
+  );
+};
+
+export default NoteForm;
