653901af2d08f7f3b40fc1c43e6bc8a2ff6f8ff7
Lisbel Cruz <Lcruztaveras@gmail.com>
2025-03-18 16:13:51 -0500

    Update NoResult component with showCta prop

--- src/components/NoResult/NoResult.tsx
+++ src/components/NoResult/NoResult.tsx
@@ -1,16 +1,23 @@
+import { FC } from "react";
 import { Flex, Text, Button } from "@chakra-ui/react";
 import NoResultIcon from "./noResultIcon.svg";
 import { NavLink as RouterNavLink } from "react-router-dom";
 
-const NoResult = () => {
+type Props = {
+  showCta?: boolean;
+};
+
+const NoResult: FC<Props> = ({ showCta }) => {
   return (
     <Flex alignItems="center" flexDirection="column">
       <Flex flexDirection="column" gap="md" alignItems="center">
         <Text>You haven't created any notes yet. Begin capturing your ideas today!</Text>
 
-        <RouterNavLink to="/details">
-          <Button background="red.100">Create Note</Button>
-        </RouterNavLink>
+        {showCta && (
+          <RouterNavLink to="/details">
+            <Button background="red.100">Create Note</Button>
+          </RouterNavLink>
+        )}
       </Flex>
       <object aria-label="plus" type="image/svg+xml" width="600px" data={NoResultIcon} />
     </Flex>
