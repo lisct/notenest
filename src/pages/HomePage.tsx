08553dc8920bae733f55e8539a534c8ea12237fb
Lisbel Cruz <Lcruztaveras@gmail.com>
2025-03-18 11:23:41 -0500

    Add PageContainer component into HopePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
 import { Heading } from "@chakra-ui/react";
+import PageContainer from "../components/PageContainer/PageContainer";
 
 const HomePage = () => {
   return (
-    <>
+    <PageContainer>
       <Heading size="2xl" color="red.100">
         Home Page
       </Heading>
-    </>
+    </PageContainer>
   );
 };
 
